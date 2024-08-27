23597a8588d6aaa61b5e36b7722d0df5ea928811
ranjaniVijayraj <ranjanivofficial@gmail.com>
2024-08-27 15:37:15 +0530

    first program

---
+++ meenaproject.java
@@ -0,0 +1,11 @@
+package kamalipackage;
+
+public class meenaproject {
+
+	public static void main(String[] args)
+	{
+		System.out.println("sisters :"+" "+"kamali" );
+		System.out.println("          "+"ranjani");
+	}
+
+}
